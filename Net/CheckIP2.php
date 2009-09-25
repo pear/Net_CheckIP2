3304056d5b86a9f7259caaabeaf8829721075d52
Till Klampaeckel <till@php.net>
2009-09-25 09:54:02 +0000

    * added isValid()
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_CheckIP2/trunk@288734 c90b9560-bf6c-de11-be94-00142212c4b1

--- Net/CheckIP2.php
+++ Net/CheckIP2.php
@@ -3,22 +3,22 @@
  * Copyright (c) 2002-2006 Martin Jansen
  *
  * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the 
- * "Software"), to deal in the Software without restriction, including 
- * without limitation the rights to use, copy, modify, merge, publish, 
- * distribute, sublicense, and/or sell copies of the Software, and to 
- * permit persons to whom the Software is furnished to do so, subject to 
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
  *
- * The above copyright notice and this permission notice shall be included 
+ * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
  * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
  */
@@ -29,7 +29,7 @@
  * Usage:
  *   <?php
  *   require_once 'Net/CheckIP2.php';
- *     
+ *
  *   if (Net_CheckIP2::check_ip("your_ip_goes_here")) {
  *       // Syntax of the IP is ok
  *   }
@@ -60,11 +60,10 @@ class Net_CheckIP2
      * If all 4 parameters pass this test, the function
      * returns true.
      *
-     * @access static
      * @param  string $ip IP adress
      * @return bool   true if syntax is valid, otherwise false
      */
-    static function check_ip($ip = '')
+    public static function check_ip($ip = '')
     {
         if (empty($ip)) {
             return false;
@@ -84,5 +83,17 @@ class Net_CheckIP2
         }
         return true;
     }
+
+    /**
+     * A better pattern.
+     *
+     * @param string $ip The IP address.
+     *
+     * @return boolean
+     * @uses   self::check_ip()
+     */
+    public static function isValid($ip)
+    {
+        return self::check_ip($ip);
+    }
 }
-?>
